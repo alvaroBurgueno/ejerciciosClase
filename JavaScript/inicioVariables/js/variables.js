1861720a28b9406f476914a40350f087d558818e
Alvaro <alburg@hotmail.es>
2017-12-07 09:27:21 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ JavaScript/inicioVariables/js/variables.js
@@ -0,0 +1,48 @@
+
+/*
+Mi nombre es ________.
+Nací el DD de MM de YYYY.
+Por tanto tengo _____ años.
+Así que he vivido _____ días.
+*/
+var myName = "Álvaro";
+
+var birthday;
+var today;
+
+/*if (){
+
+} else {
+
+}*/
+
+
+var today = new Date();  
+var birthday = new Date(1988,06,30); 
+
+var diaNac = birthday.getDate(); 
+var mesNac = birthday.getMonth();
+var anioNac = birthday.getFullYear();
+
+var myAge = today.getFullYear() - birthday.getFullYear(); 
+
+var myTime = (today - birthday)/86400000;
+
+
+console.log("Mi nombre es "+myName+".");
+console.log("Nací el "+diaNac+" de "+mesNac+" de "+anioNac+".");
+console.log("Por tanto tengo "+myAge);
+console.log("Así que he vivido "+myTime+" días.");
+
+			
+
+
+
+
+
+
+
+
+
+
+
---
+++ Odissey/js/scripts.js
@@ -0,0 +1,22 @@
+
+
+var person; /*declarar Variable*/
+
+
+var num1=1;
+var num2=2;
+resultado= num2+num1;
+totalDays = 45;
+person1 = "Álvaro"; /* Asignar Valor a la Variable.*/
+person2 = "Elisa";
+console.log("Hello World "+person1+" y "+person2); /*Salida*/
+console.log("El resultado es "+resultado);
+console.log( "Hoy hace " + totalDays + " días que estáis juntos." );
+
+  usuario=prompt('mete algo cabrón!!:','');
+
+  prompt("Has escrito "+usuario);
+
+console.log(person2 + 2);
+
+
